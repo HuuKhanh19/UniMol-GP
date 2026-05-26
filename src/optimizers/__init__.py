e11360e66849b9e0b79553373cd3f3cd9c0d129a
HuuKhanh19 <hkhanhp19@gmail.com>
2026-05-27 00:20:17 +0700

    unimolv1 goc

--- src/optimizers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-CONAN Project Optimizers
-
-Contains EGGROLL (Evolution Strategies with Low-Rank Perturbations) implementation.
-"""
-
-from .eggroll import EGGROLL, EGGROLLConfig
-
-__all__ = ['EGGROLL', 'EGGROLLConfig']
